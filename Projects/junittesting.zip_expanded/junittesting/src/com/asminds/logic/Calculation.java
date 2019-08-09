1f88368ed1500600c0e512f08d0937491c042929
rajue <yogarani2014@gmail.com>
2019-08-09 19:30:24 +0530

    Welcome line added in Calculation Class

--- Projects/junittesting.zip_expanded/junittesting/src/com/asminds/logic/Calculation.java
+++ Projects/junittesting.zip_expanded/junittesting/src/com/asminds/logic/Calculation.java
@@ -16,6 +16,7 @@ public static void main(String[] args)
 	Scanner s=new Scanner(System.in);
 	System.out.println("Enter the Factorial number: ");
 	n=s.nextInt();
+	System.out.println("Welcome");
 System.out.println("Factorial value is:" +factorial(n) );  
 }  
 	}
