728aae9c681708986ae61ba9981b90ba052ba853
rajue <yogarani2014@gmail.com>
2019-08-09 19:26:37 +0530

    Junit Project added

---
+++ Projects/junittesting.zip_expanded/junittesting/src/com/asminds/logic/Calculation.java
@@ -0,0 +1,22 @@
+package com.asminds.logic;
+
+import java.util.Scanner;
+
+public class Calculation {
+	public static int factorial(int n){      
+         if (n == 1)      
+           return 1;      
+         else      
+           return(n * factorial(n-1));      
+   }      
+ 
+public static void main(String[] args)
+{ 
+	int n;
+	Scanner s=new Scanner(System.in);
+	System.out.println("Enter the Factorial number: ");
+	n=s.nextInt();
+System.out.println("Factorial value is:" +factorial(n) );  
+}  
+	}
+
---
+++ Projects/junittesting.zip_expanded/junittesting/src/com/asminds/test/TestCase.java
@@ -0,0 +1,15 @@
+package com.asminds.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.asminds.logic.Calculation;
+
+public class TestCase {
+@Test
+public void testPrime() {
+	assertEquals(6, Calculation.factorial(3));
+	
+}
+}
